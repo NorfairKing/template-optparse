cc7e1dc142a19b55c111793872ae20cbeadceeb7
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-10-13 22:12:33 +0200

    Big block of instructions

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # Optparse Template
 
-**Under construction**
+This is a template implementation of commands, flags, options, environment variable and configuration file parsing according to best practices.
 
 ## License
 
-This code is **not** free to use.
-See https://template.cs-syd.eu for more information.
+This template is **not** free to use.
+See https://template.cs-syd.eu/template/NorfairKing/template-optparse for more information.
 
 Copyright (c) 2020 Tom Sydney Kerckhove.
 
 All Rights Reserved.
 
+## Instructions
+
+You can build this template code using `stack build` according to the provided `stack.yaml` file.
+You can also use cabal if you prefer, as long as you can get the appropriate dependencies in place.
+
+To use this template, copy the `src/OptParse.hs` and `test/OptParseSpec.hs` modules into your project and follow instructions inside.
+
+Run `stack haddock` to read the instructions in haddock form.
+Otherwise, continue reading in [the source](src/OptParse.hs).
--- src/OptParse.hs
+++ src/OptParse.hs
@@ -4,6 +4,77 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications #-}
 
+-- | = Optparse Template
+--
+-- This is a template implementation of commands, flags, options, environment variable and configuration file parsing according to best practices.
+-- To use this template, follow the instructions below and delete anything you do not need.
+--
+-- == License
+--
+-- This template is __not__ free to use.
+--
+-- See https://template.cs-syd.eu/template/NorfairKing/template-optparse for more information.
+--
+-- Copyright (c) 2020 Tom Sydney Kerckhove.
+--
+-- All Rights Reserved.
+--
+-- == Instructions
+--
+-- === Purpose
+--
+-- The purpose of this module is to determine everything that is needed for the workings of your program.
+-- This includes dealing with command-line arguments, flags and options, environment variables and configuration files.
+-- This information is gathered in an 'Instructions' type, which you can get using the `getInstructions` function.
+-- After this information is gathered, your program should never have to consult the arguments, environment variables or configuration file anymore.
+--
+-- === Glossary
+--
+-- In what follows, I will use this terminology:
+--
+-- * Commands, Flags, Options: Whatever you put after the command on the command-line.
+-- * Environment: Environment variables
+-- * Configuration: Whatever you find in the configuration file
+--
+-- For the purposes of this code, we use the following types:
+--
+-- * 'Command': A sum type for the commands, each with their own flags and options.
+-- * 'Flags': A product type for the flags and options that are common across commands.
+-- * 'Arguments': The 'Command' and 'Flags' together.
+-- * 'Environment': A product type for the relevant environment variables.
+-- * 'Configuration': A product type for the relevant parts of the configuration file.
+-- * 'Dispatch': A sum type for the commands, each with their own settings.
+-- * 'Settings': A product type for the settings that are command across commands.
+-- * 'Instructions': The 'Dispatch' and 'Settings' together.
+--
+-- === High-level workings
+--
+-- The gathering of the 'Instructions' happens in two stages.
+--
+-- In the first stage, the 'Arguments', 'Environment' and 'Configuration' values are gathered from their respective places.
+-- At this point, these types should represent what is found, without any processing or validation.
+--
+-- In the second stage, in the `combineToInstructions` function, all these variables are combined together to produce the 'Instructions'.
+-- It is in this phase that we do processing, validation and even 'System.Exit.die' if necessary.
+--
+-- === 'FilePath' Example
+--
+-- As an example, suppose our program uses a cache file for its 'compute' command.
+--
+-- 1. Hhave a constructor for the 'compute' command that in the 'Command' sum type: 'CommandCompute'
+-- 2. Add a 'ComputeFlags' type that contains a 'commandFlagCacheFile :: Maybe FilePath' field to indicate that the user may specify this file on the command-line.
+-- 3. Add a field to the 'Environment' type to indicate that the user may specify this file in an environment variable.
+-- 4. Add a field to the 'Configuration' type to indicate that the user may specify this file in the configuration file as well.
+-- 5. Combine the above three in the 'combineToInstructions' function.
+--
+--
+-- === Further instructions and support
+--
+-- Continue reading in the code inline for more detailed instructions.
+--
+-- You can see an example of option parsing according to this template at https://github.com/NorfairKing/intray/blob/master/intray-cli/src/Intray/Cli/OptParse.hs
+--
+-- If you have any trouble, you can contact @syd@ at @cs-syd@ dot @eu@ for support.
 module OptParse where
 
 import Control.Applicative
